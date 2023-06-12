5365b1fae7ffeb0d6e54f6e5e0e14d1c1c7c52e3
Vasudev Shekhar <108407563+VasudevShekhar@users.noreply.github.com>
2023-06-12 09:17:35 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["https://technotes-api-zbtd.onrender.com", "https://technote-crud.onrender.com", "https://technotes-31ea.onrender.com"];
+const allowedOrigins = ["https://technotes-31ea.onrender.com", "https://technotes.onrender.com" ,"https://technotes-api-zbtd.onrender.com", "https://technote-crud.onrender.com"];
 
 module.exports = allowedOrigins;
